a9f4da9131ffed99a1640072af5d262b22e9a869
holydk <omigoneo@gmail.com>
2021-02-20 16:41:55 +0300

    issue 5349 Add 'City' into EstimateShippingModel

--- src/Presentation/Nop.Web/Models/ShoppingCart/EstimateShippingModel.cs
+++ src/Presentation/Nop.Web/Models/ShoppingCart/EstimateShippingModel.cs
@@ -20,6 +20,7 @@ namespace Nop.Web.Models.ShoppingCart
         public int? CountryId { get; set; }
         public int? StateProvinceId { get; set; }
         public string ZipPostalCode { get; set; }
+        public string City { get; set; }
         
         public IList<SelectListItem> AvailableCountries { get; set; }
         public IList<SelectListItem> AvailableStates { get; set; }
